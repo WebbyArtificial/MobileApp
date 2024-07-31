89773757279283ffc29f33ccf6d5c257a00ab6e8
Andrea Cammarata <a.cammarata@webbymarketing.it>
2024-07-31 09:27:37 +0200

    Primi test

--- MobilePrototype/AppShell.xaml.cs
+++ MobilePrototype/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace MobilePrototype
+﻿
+using MobilePrototype.Pages;
+
+namespace MobilePrototype
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("LoginPage", typeof(LoginPage));
         }
     }
 }
--- MobilePrototype/MainPage.xaml.cs
+++ MobilePrototype/MainPage.xaml.cs
@@ -1,8 +1,7 @@
-﻿namespace MobilePrototype
+﻿namespace MobilePrototype.Pages
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
@@ -11,14 +10,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Shell.Current.GoToAsync("LoginPage");
         }
     }
 
